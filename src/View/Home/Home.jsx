ad67936c19b7189160a4aa4d0b523d15a0e90632
rSaydelleS <rafael.ppuerari@gmail.com>
2025-10-30 22:24:03 -0300

    adicionando a configuracao do yml

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -1,58 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import './index.scss';
-import { info } from '../../API/api';
-import HomeCard from '../HomeCard/HomeCard';
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { info } from "../../API/api";
+import HomeCard from "../HomeCard/HomeCard";
 
 function Home() {
+  const [Char, setChar] = useState([]);
+  const [list, setList] = useState(1);
 
-const [Char, setChar] = useState([]);
-const [list, setList] = useState(1);
+  async function infos() {
+    const {
+      data: { results },
+    } = await info.getCharacters(list);
+    setChar(results);
+  }
+  useEffect(() => {
+    infos();
+  }, infos());
 
-async function infos() {
-  const {data: {results},} = await info.getCharacters(list);
-  setChar(results);
-}
-useEffect(() =>{
-  infos();
-},[list])
+  function previousPage(e) {
+    e.preventDefault();
+    setList(list - 1);
+  }
+  function nextPage(e) {
+    e.preventDefault();
+    setList(list + 1);
+  }
 
-function previousPage(e) {
-  e.preventDefault();
-  setList(list - 1);
-}
-function nextPage(e) {
-  e.preventDefault();
-  setList(list + 1);
-}
-
-async function searchCaracter(event) {
-  event.preventDefault();
-  const value = event.target[0].value;
-  const {data: {results}} = await info.getChar(value);
-  setChar(results);
-}
-  return <>
-  <div className='search'>
-      <form onSubmit={searchCaracter}>
-        <input placeholder='Pesquise o personagem' type='text'></input>
-        <button type='submit'>Pesquisar!</button>
-      </form>  
-  </div>
-  <div className="all">
-      <div className='modal'>
-            {Char.map((Char) =>(
-              <div key={Char.id}>
-                <HomeCard Char={Char}/>
-              </div>
-            ))}
+  async function searchCaracter(event) {
+    event.preventDefault();
+    const value = event.target[0].value;
+    const {
+      data: { results },
+    } = await info.getChar(value);
+    setChar(results);
+  }
+  return (
+    <>
+      <div className="search">
+        <form onSubmit={searchCaracter}>
+          <input placeholder="Pesquise o personagem" type="text"></input>
+          <button type="submit">Pesquisar!</button>
+        </form>
       </div>
-      <div className='chosepage'>
+      <div className="chosepage">
         <button onClick={previousPage}>Página anterior</button>
         <p>Página atual : {list}</p>
         <button onClick={nextPage}>Próxima página</button>
       </div>
-    </div>  
-  </>
+      <div className="all">
+        <div className="modal">
+          {Char.map((Char) => (
+            <div key={Char.id}>
+              <HomeCard Char={Char} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home  
+export default Home;
